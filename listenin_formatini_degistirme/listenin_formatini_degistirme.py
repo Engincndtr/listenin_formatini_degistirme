11c35376dc484cbd8b3bfb969bf30a625bfdd00c
Engincndtr <engin.dutar.ecd@gmail.com>
2024-07-24 02:36:39 +0300

    listenin_formatini_degistirme
    Bir liste olusturup formatini set, tuple ve sozluk olarak yazdirmaya yarayan kod. Python ile gelistirildi.

---
+++ listenin_formatini_degistirme/listenin_formatini_degistirme.py
@@ -0,0 +1,19 @@
+liste = [input("Bos listeye eleman giriniz: ")]
+set1 = set(liste)
+tuple1 = tuple(liste)
+sozluk = {
+    "Sozluk1": liste
+          }
+print("liste bicimi=",liste)
+print("set bicimi icin=",set1)
+print("tuple bicimi=",tuple1)
+print("sozluk bicimi=",sozluk)
+
+
+
+
+
+
+
+
+
